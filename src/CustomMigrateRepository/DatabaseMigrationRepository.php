c9da9e1f0eccb41d67e0cca7db7c9f3b483f7485
Alexander Cerleniuk <sano1982@gmail.com>
2014-11-29 20:11:45 +0200

    formatting

--- src/CustomMigrateRepository/DatabaseMigrationRepository.php
+++ src/CustomMigrateRepository/DatabaseMigrationRepository.php
@@ -1,45 +1,47 @@
 <?php
+
 namespace CustomMigrateRepository;
 
 use Carbon\Carbon;
 use Illuminate\Database\Migrations\DatabaseMigrationRepository as OriginalRepository;
 
-class DatabaseMigrationRepository extends OriginalRepository {
+class DatabaseMigrationRepository extends OriginalRepository
+{
 
-	/**
-	 * Log that a migration was run.
-	 *
-	 * @param  string  $file
-	 * @param  int     $batch
-	 * @return void
-	 */
-	public function log($file, $batch)
-	{
+    /**
+     * Log that a migration was run.
+     *
+     * @param  string  $file
+     * @param  int     $batch
+     * @return void
+     */
+    public function log($file, $batch)
+    {
         $time = new Carbon;
         $record = array('migration' => $file, 'batch' => $batch, 'created_at' => $time);
 
-		$this->table()->insert($record);
-	}
+        $this->table()->insert($record);
+    }
 
-	/**
-	 * Create the migration repository data store.
-	 *
-	 * @return void
-	 */
-	public function createRepository()
-	{
-		$schema = $this->getConnection()->getSchemaBuilder();
+    /**
+     * Create the migration repository data store.
+     *
+     * @return void
+     */
+    public function createRepository()
+    {
+        $schema = $this->getConnection()->getSchemaBuilder();
 
-		$schema->create($this->table, function($table)
-		{
-			// The migrations table is responsible for keeping track of which of the
-			// migrations have actually run for the application. We'll create the
-			// table to hold the migration file's path as well as the batch ID.
-			$table->string('migration');
+        $schema->create($this->table, function($table) {
+            // The migrations table is responsible for keeping track of which of the
+            // migrations have actually run for the application. We'll create the
+            // table to hold the migration file's path as well as the batch ID.
+            $table->string('migration');
 
-			$table->integer('batch');
+            $table->integer('batch');
 
             $table->timestamp('created_at');
-		});
-	}
+        });
+    }
+
 }
--- src/CustomMigrateRepository/MigrationServiceProvider.php
+++ src/CustomMigrateRepository/MigrationServiceProvider.php
@@ -1,23 +1,25 @@
 <?php
+
 namespace CustomMigrateRepository;
 
 use CustomMigrateRepository\DatabaseMigrationRepository;
 use Illuminate\Database\MigrationServiceProvider as OriginalMigrationServiceProvider;
 
-class MigrationServiceProvider extends OriginalMigrationServiceProvider {
+class MigrationServiceProvider extends OriginalMigrationServiceProvider
+{
 
-	/**
-	 * Register the migration repository service.
-	 *
-	 * @return void
-	 */
-	protected function registerRepository()
-	{
-		$this->app->bindShared('migration.repository', function($app)
-		{
-			$table = $app['config']['database.migrations'];
+    /**
+     * Register the migration repository service.
+     *
+     * @return void
+     */
+    protected function registerRepository()
+    {
+        $this->app->bindShared('migration.repository', function($app) {
+            $table = $app['config']['database.migrations'];
 
-			return new DatabaseMigrationRepository($app['db'], $table);
-		});
-	}
+            return new DatabaseMigrationRepository($app['db'], $table);
+        });
+    }
+
 }
